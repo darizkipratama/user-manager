59c7ae947b5b8a8ca0cd62835fec5f53c29c977c
darizkipratama <dpratama@gmail.com>
2025-02-13 20:04:29 +0700

    feat(web): update Firebase project ID in auth configuration

--- apps/web/app/config/authConfig.ts
+++ apps/web/app/config/authConfig.ts
@@ -4,7 +4,7 @@ import { getAuth, connectAuthEmulator } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'backend-repo-b2133',
   storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
   appId: 'YOUR_APP_ID',
